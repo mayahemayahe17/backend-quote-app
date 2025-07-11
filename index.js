f5da617773ac4bbffbfdbf108d9324e1a9c781c4
mayahemayahe17 <159863545+mayahemayahe17@users.noreply.github.com>
2025-07-12 11:51:56 +1200

    Add dotenv for environment variable management

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // 配置 CORS，允许来自指定前端端口的请求
 app.use(
   cors({
-    origin: "http://localhost:3001", // 允许来自前端的请求（前端运行在3001端口）
+    origin: "https://scaffolding-quote-app.vercel.app", // 允许来自前端的请求（前端运行在3001端口）
     methods: ["GET", "POST", "PUT", "DELETE"], // 允许的方法
   })
 );
@@ -25,8 +25,7 @@ createTables();
 // 使用 /quote 路由
 app.use("/quote", quoteRoutes);
 
-// 启动服务器
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // 如果没有环境变量 PORT，则回退到 3000
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
